874b4defb81fe11a109b557cf2c555a0b3691be9
Maurice Makaay <maurice@h2b.nl>
2025-12-07 23:37:08 +0100

    Golf the heck out of 7.2

--- 07_Laboratories/golf2.py
+++ 07_Laboratories/golf2.py
@@ -1,7 +1,5 @@
 import functools as f
 l=tuple(open(0));w=len(l[0])-1
 @f.cache
-def B(x,y=len(l)-1,r=0):
- if y:y-=1;r=(l[y][x]=="S")+B(x-1,y)*(l[y][x-1]>"S")+B(x,y)*(l[y][x]<"S")+(B(x+1,y) if l[y][x+1]>"S" else 0)
- return r
+def B(x,y=len(l)-1):y-=1;u=l[y];return (u[x]=="S")+B(x-1,y)*(u[x-1]>"S")+B(x,y)*(u[x]<"S")+(B(x+1,y)if u[x+1]>"S"else 0)if~y else 0
 print(sum(map(B,range(w))))
