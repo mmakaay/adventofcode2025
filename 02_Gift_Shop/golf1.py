150168e94c423e618dc37cd2601b725d8e5ddd07
Maurice Makaay <maurice@h2b.nl>
2025-12-08 10:12:17 +0100

    Shave a few bytes of day 2 golf

--- 02_Gift_Shop/golf1.py
+++ 02_Gift_Shop/golf1.py
@@ -1,7 +1,5 @@
 import re
 T=0
 for a,b in[tuple(map(int,r.split("-")))for r in next(open(0)).split(",")]:
- while a<=b:
-  if re.match(r"^(\d+)\1$",str(a)):T+=a
-  a+=1
+ while a<=b:T+=bool(re.match(r"^(\d+)\1$",str(a)))*a;a+=1
 print(T)
