4902a7bdc1afd91aacbd07ce8f39c1c1c9d9b901
Maurice Makaay <maurice@h2b.nl>
2025-12-08 21:14:59 +0100

    Yeah, strip one byte from both day 8 golfs

--- 08_Playground/golf1.py
+++ 08_Playground/golf1.py
@@ -1,5 +1,5 @@
 import math,collections
 l,B=len(s:=[eval(x)for x in open(0)]),{}
 for _,a,b in sorted((math.dist(s[i],s[j]),s[i],s[j])for i in range(l)for j in range(i+1,l))[:1000]:
- x,y=B.get(a,a),B.get(b,b);B={n:[B[n],x][B[n]==y] for n in B};B[a]=B[b]=x
+ x,y=B.get(a,a),B.get(b,b);B={n:[B[n],x][B[n]==y]for n in B};B[a]=B[b]=x
 a,b,c=sorted(collections.Counter(B.values()).values())[-3:];print(a*b*c)
