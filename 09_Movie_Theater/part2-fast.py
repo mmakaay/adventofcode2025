3afdd1240590d95b4a5aeec8a4bc71f657d61265
Maurice Makaay <maurice@h2b.nl>
2025-12-10 09:25:27 +0100

    Another optimization, by handling TL->BR rectnagle normalization early

--- 09_Movie_Theater/part2-fast.py
+++ 09_Movie_Theater/part2-fast.py
@@ -14,31 +14,28 @@ nr_of_red_tiles = len(red_tile_coordinates)
 #
 # Build the polygon edges from consecutive red tiles. Since the red tiles
 # are already in order, we just connect each tile to the next one.
-polygon_edges = list(sorted(
-    zip(
-        red_tile_coordinates,
-        red_tile_coordinates[1:] + red_tile_coordinates[:1]
-    ),
-    # This sorting gives me the best results. This might be related
-    # to my specific data set.
-    key=lambda a: a[1][0]-a[0][0],
-))
+polygon_edges = [
+    ((min(x1, x2), min(y1, y2)), (max(x1, x2), max(y1, y2)))
+    for (x1,y1), (x2,y2)
+    in sorted(
+        zip(
+            red_tile_coordinates,
+            red_tile_coordinates[1:] + red_tile_coordinates[:1]
+        ),
+        # This sorting gives me the best results. This might be related
+        # to my specific data set.
+        key=lambda a: a[1][0]-a[0][0],
+    )
+]
 
 
 def edge_crosses_rectangle(edge, min_x, max_x, min_y, max_y):
     (x1, y1), (x2, y2) = edge
 
-    # Vertical edge.
     if x1 == x2:
-        x = x1
-        y_from, y_to = min(y1, y2), max(y1, y2)
-        return min_x < x < max_x and y_to > min_y and y_from < max_y
-
-    # Horizontal edge.
+        return min_x < x1 < max_x and y2 > min_y and y1 < max_y
     else:
-        y = y1
-        x_from, x_to = min(x1, x2), max(x1, x2)
-        return y > min_y and y < max_y and x_to > min_x and x_from < max_x
+        return min_y < y1 < max_y and x2 > min_x and x1 < max_x
 
 
 def is_rectangle_valid(p1, p2):
@@ -56,14 +53,12 @@ def is_rectangle_valid(p1, p2):
 
 # Search for the largest possible rectangle.
 best_area = 0
-for i, j in [
-    (i, j)
-    for i in range(nr_of_red_tiles)
-    for j in range(i+1, nr_of_red_tiles)
+for p1, p2 in [
+    (p1, p2)
+    for p1 in red_tile_coordinates
+    for p2 in red_tile_coordinates
+    if p1 < p2
 ]:
-    p1 = red_tile_coordinates[i]
-    p2 = red_tile_coordinates[j]
-
     # Skip if smaller than the currently best area.
     area = (abs(p2[0] - p1[0]) + 1) * (abs(p2[1] - p1[1]) + 1)
     if area <= best_area:
