2dc7e66a72b07d176f0dfc4a0485b36799d0e3a1
Maurice Makaay <maurice@h2b.nl>
2025-12-14 11:46:37 +0100

    Cleanup

--- 01_Secret_Entrance/golf2.2.py
+++ 01_Secret_Entrance/golf2.2.py
@@ -2,4 +2,3 @@ Z,P=0,50
 for L in open(0):
  for _ in range(int(L[1:])):Z+=(P:=(P+1-2*(L[0]<'M'))%100)<1
 print(Z)
-
--- 12_Christmas_Tree_Farm/golf1.rb
+++ 12_Christmas_Tree_Farm/golf1.rb
@@ -1,1 +1,1 @@
-p$<.count{_1[/x/]&&(a=_1.split;9*a[1..].sum(&:to_i)<=eval(a[0][...-1].tr('x','*'))+2)}
+p$<.count{_1[/x/]&&(a=_1.split;9*a[1..].sum(&:to_i)<=eval(a[0][...-1].tr('x','*')))}
